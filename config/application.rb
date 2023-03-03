ee799b4428a0db24838f3b05a3a7800897596fd6
erickhansx <erickhansx@gmail.com>
2023-03-03 16:57:35 -0600

    Fix bugs - authentications

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,9 @@ module PetbnbBack
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.session_store :cookie_store, key: '_your_app_session'
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
 
   end
 end
