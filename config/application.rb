1d210d2c525b07a35723a2008515a63afe943fb3
lana <diegogarciaq98@gmail.com>
2023-03-03 17:39:51 -0500

    Bug fix signup authentication

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module PetbnbBack
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.session_store :cookie_store, key: '_your_app_session'
+
   end
 end
